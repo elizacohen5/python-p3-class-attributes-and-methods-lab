4d8a0dbb9715915e68b7863806656252074a23f5
Eliza Cohen <elizacohen5@gmail.com>
2024-06-06 09:56:42 -0400

    complete python-p3-class-attributes-and-methods-lab

--- lib/song.py
+++ lib/song.py
@@ -1,2 +1,60 @@
 class Song:
-    pass
+    count = 0
+    genres = []
+    artists = []
+    genre_count = {}
+    artist_count = {}
+
+    def __init__(self, name, artist, genre):
+        self.name = name
+        self.artist = artist
+        self.genre = genre
+        self.increase_song_count()
+        self.add_to_genres(self)
+        self.add_to_artists(self)
+        self.add_to_genre_count(self)
+        self.add_to_artist_count(self)
+    
+    @classmethod
+    def increase_song_count(cls, increment=1):
+        cls.count += increment
+
+    @classmethod
+    def add_to_genres(cls, self):
+        if self.genre not in cls.genres:
+            cls.genres.append(self.genre)
+    
+    @classmethod
+    def add_to_artists(cls, self):
+        if self.artist not in cls.artists:
+            cls.artists.append(self.artist)
+    
+    @classmethod
+    def add_to_genre_count(cls, self):
+        if self.genre in cls.genre_count:
+            cls.genre_count[self.genre] += 1
+        else:
+            cls.genre_count[self.genre] = 1
+    
+    @classmethod
+    def add_to_artist_count(cls, self):
+        if self.artist in cls.artist_count:
+            cls.artist_count[self.artist] += 1
+        else:
+            cls.artist_count[self.artist] = 1
+
+
+
+    
+baby = Song('baby', 'justin beiber', 'pop')
+judas = Song('judas', 'lady gaga', 'pop')
+back_in_black = Song('back in black', 'acdc', 'rock')
+
+print(Song.genres)
+print(Song.artists)
+print(Song.genre_count)
+print(Song.artist_count)
+
+
+        
+        
